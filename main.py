d1b6bb4bf9847fa4799fe7aa9fb7ba9c58d41721
Erumpet <erumpet@gmail.com>
2017-06-13 00:00:34 -0500

    Switched from a static counter to a queue based system for tracking a users messages.

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import sys
 import time
 import telepot
+from collections import deque
 from telepot.loop import MessageLoop
 from telepot.delegate import pave_event_space, per_chat_id, create_open
 
 # MrHush's telegram authentication token
-TOKEN = '341016958:AAFm44_Qjo5Z1cZGT375_mhnCdvizGAMad4'
+TIMEOUT = 35
+TOKEN = ''
+with open(sys.argv[1], 'r') as f:
+    TOKEN = f.readline().strip()
 
 class MessageCounter(telepot.helper.ChatHandler):
 	def __init__(self, *args, **kwargs):
 		super(MessageCounter, self).__init__(*args, **kwargs)
-		self._count = {}
+		self._messages = {}
 		self._alert = {}
 
 	def on_chat_message(self, msg):
-		# Extract meta data
+		# Extract message data
 		content_type, chat_type, chat_id = telepot.glance(msg)
 		from_block = msg['from']
 		user_id = from_block['id']		
 		user_first = from_block['first_name']
+		time = msg['date']
 
-		# Count messages
-		if user_id in self._count:
-			self._count[user_id] += 1
+		# Track message
+		if user_id in self._messages:
+			self._messages[user_id].append(time)
 		else:
-			self._count[user_id] = 1
+			self._messages[user_id] = deque([time])
 			self._alert[user_id] = True
+
+		# Remove stale messages
+		while (time - self._messages[user_id][0]) > TIMEOUT:
+			self._messages[user_id].popleft()
 
 		# Top quality Sean meme
 		if user_first == 'Sean' and msg['text'] == 'sucky sucky, Ching pong ping':
@@ -33,18 +42,19 @@ class MessageCounter(telepot.helper.ChatHandler):
 			self.sender.sendMessage('Mistah gimme five dollah!', disable_notification = True)
 
 		# Check if excessive shitposting is occurring
+
 		# First give a warning
-		if self._count[user_id] >= 7 and self._alert[user_id]:
+		if len(self._messages[user_id]) >= 7 and self._alert[user_id]:
 			self.sender.sendMessage('Hey ' + user_first + ', slow your roll kid, you\'re shitposting over the legal limit.', disable_notification = True)
 			self._alert[user_id] = False
 
 		# Start deleting messages
-		if self._count[user_id] >= 8 and chat_type == 'supergroup':
+		if len(self._messages[user_id]) >= 8 and chat_type == 'supergroup':
 			msg_id = telepot.message_identifier(msg)
 			self.bot.deleteMessage(msg_id)
 
 		# Ban
-		if self._count[user_id] >= 15 and chat_type == 'supergroup':
+		if len(self._messages[user_id]) >= 15 and chat_type == 'supergroup':
 			self.bot.kickChatMember(chat_id, user_id)
 			self.sender.sendMessage('Banned.', disable_notification = True)
 
