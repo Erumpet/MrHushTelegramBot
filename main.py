29cfa6954bc89aacf005afbe163cc066e3e53fe5
Erumpet <erumpet@gmail.com>
2017-06-12 23:01:39 -0500

    cleaned up a little

--- main.py
+++ main.py
@@ -7,21 +7,6 @@ from telepot.delegate import pave_event_space, per_chat_id, create_open
 # MrHush's telegram authentication token
 TOKEN = '341016958:AAFm44_Qjo5Z1cZGT375_mhnCdvizGAMad4'
 
-'''
-def handle(msg):
-	content_type, chat_type, chat_id = telepot.glance(msg)
-	print(content_type, chat_type, chat_id)
-	print(msg['from'])
-
-	if content_type == 'text':
-		bot.sendMessage(chat_id, msg['text'])
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
-while 1:
-	time.sleep(10);
-
-'''
 class MessageCounter(telepot.helper.ChatHandler):
 	def __init__(self, *args, **kwargs):
 		super(MessageCounter, self).__init__(*args, **kwargs)
@@ -42,12 +27,13 @@ class MessageCounter(telepot.helper.ChatHandler):
 			self._count[user_id] = 1
 			self._alert[user_id] = True
 
-		# Check if excessive shitposting is occurring
-		# First give a warning
+		# Top quality Sean meme
 		if user_first == 'Sean' and msg['text'] == 'sucky sucky, Ching pong ping':
 			self.sender.sendMessage('*sucks Sean\'s little teeny tiny ding dong*', disable_notification = True)
 			self.sender.sendMessage('Mistah gimme five dollah!', disable_notification = True)
 
+		# Check if excessive shitposting is occurring
+		# First give a warning
 		if self._count[user_id] >= 7 and self._alert[user_id]:
 			self.sender.sendMessage('Hey ' + user_first + ', slow your roll kid, you\'re shitposting over the legal limit.', disable_notification = True)
 			self._alert[user_id] = False
@@ -71,29 +57,5 @@ bot = telepot.DelegatorBot(TOKEN, [
 MessageLoop(bot).run_as_thread()
 while 1:
 	time.sleep(15)
-
-
-'''
-8450 okinawa st sacremrrnto 95828
-bt collins army reserve training facility
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
